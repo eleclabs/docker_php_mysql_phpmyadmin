0d6c941712867f96d8abc1a9616239a4f84304f0
eleclabs <eleclabs@gmail.com>
2025-08-19 10:58:16 +0700

    first commit

---
+++ php/src/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Hello World!</h1>
+    <p>ยินดีต้อนรับเข้าสู่เว็บไซต์ของฉัน </p>
+    <p>Design by Somkiat </p>
+
+</body>
+</html>
